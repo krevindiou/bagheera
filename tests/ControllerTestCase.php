b0c25c6d889ca680eb5bf9a68e8a5bb3225782be
krevindiou <krevindiou@users.noreply.github.com>
2010-09-12 19:14:25 +0200

    Set up unit tests

---
+++ tests/ControllerTestCase.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the Bagheera project, a personal finance manager.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'Zend/Test/PHPUnit/ControllerTestCase.php';
+require_once 'Zend/Application.php';
+
+abstract class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase
+{
+    public $application;
+
+    public function setUp()
+    {
+        $this->application = new Zend_Application(
+            APPLICATION_ENV,
+            APPLICATION_PATH . '/configs/application.ini'
+        );
+
+        $this->bootstrap = array($this, 'appBootstrap');
+        parent::setUp();
+    }
+
+    public function appBootstrap()
+    {
+        $this->application->bootstrap();
+    }
+}
---
+++ tests/application/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(__DIR__ . '/../../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
