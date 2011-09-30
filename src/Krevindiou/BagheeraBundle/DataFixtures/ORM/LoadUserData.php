62a9cc8ca3babd3e3eee7091a057973291ae7767
krevindiou <krevindiou@users.noreply.github.com>
2011-10-01 00:05:01 +0200

    Encode passwords with sha512

--- src/Krevindiou/BagheeraBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Krevindiou/BagheeraBundle/DataFixtures/ORM/LoadUserData.php
@@ -4,17 +4,20 @@ namespace Krevindiou\BagheeraBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\AbstractFixture,
     Doctrine\Common\DataFixtures\OrderedFixtureInterface,
-    Krevindiou\BagheeraBundle\Entity\User;
+    Krevindiou\BagheeraBundle\Entity\User,
+    Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load($em)
     {
+        $encoder = new MessageDigestPasswordEncoder('sha512', false, 1);
+
         $user = new User();
         $user->setFirstname('John');
         $user->setLastname('DOE');
         $user->setEmail('john@example.net');
-        $user->setPassword('a0d540a78cd61daa5fb872ac29272c00');
+        $user->setPassword($encoder->encodePassword('john', $user->getSalt()));
         $user->setActivation('b4fa77f5180803d0f6f4f504594da09e');
         $user->setIsAdmin(true);
         $user->setIsActive(true);
@@ -25,7 +28,7 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
         $user->setFirstname('Jane');
         $user->setLastname('DOE');
         $user->setEmail('jane@example.net');
-        $user->setPassword('5844a15e76563fedd11840fd6f40ea7b');
+        $user->setPassword($encoder->encodePassword('jane', $user->getSalt()));
         $user->setActivation('a24fe4584a99123d8f38a9a4e0abae54');
         $user->setIsAdmin(false);
         $user->setIsActive(true);
