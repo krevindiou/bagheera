e50215273394b973ce4dbb2052e55b7da4cba360
krevindiou <krevindiou@users.noreply.github.com>
2012-01-14 12:31:04 +0100

    Check operation type

--- src/Krevindiou/BagheeraBundle/Form/OperationForm.php
+++ src/Krevindiou/BagheeraBundle/Form/OperationForm.php
@@ -164,6 +164,10 @@ class OperationForm extends AbstractType
                     $validator = new Assert\NotBlankValidator();
                     $constraint = new Assert\NotBlank();
 
+                    if (!$validator->isValid($form['type']->getData(), $constraint)) {
+                        $form->get('type')->addError(new FormError($constraint->message));
+                    }
+
                     if (!$validator->isValid($form['amount']->getData(), $constraint)) {
                         $form->get('amount')->addError(new FormError($constraint->message));
                     }
