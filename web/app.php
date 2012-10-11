e9b95dc0aad75d7e1827d9949fd06699a1b2e6d3
krevindiou <krevindiou@users.noreply.github.com>
2012-10-11 22:40:16 +0200

    Upgrade to Symfony 2.1

--- web/app.php
+++ web/app.php
@@ -1,12 +1,20 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
-
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+$loader = new ApcClassLoader('bagheera', $loader);
+$loader->register(true);
+
+require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-$kernel->handle(Request::createFromGlobals())->send();
+$kernel = new AppCache($kernel);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
