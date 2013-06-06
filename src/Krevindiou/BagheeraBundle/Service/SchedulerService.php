610ef4638d3c68b885c113536800d791a0bf7905
krevindiou <krevindiou@users.noreply.github.com>
2013-06-06 22:33:42 +0200

    Use SQL for list of schedulers

--- src/Krevindiou/BagheeraBundle/Service/SchedulerService.php
+++ src/Krevindiou/BagheeraBundle/Service/SchedulerService.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\FormFactory;
 use Symfony\Component\Validator\Validator;
 use Symfony\Bridge\Monolog\Logger;
 use Pagerfanta\Pagerfanta;
-use Pagerfanta\Adapter\DoctrineCollectionAdapter;
+use Pagerfanta\Adapter\CallbackAdapter;
 use JMS\DiExtraBundle\Annotation as DI;
 use Krevindiou\BagheeraBundle\Entity\User;
 use Krevindiou\BagheeraBundle\Entity\Account;
@@ -51,7 +51,100 @@ class SchedulerService
     public function getList(User $user, Account $account, $currentPage = 1)
     {
         if ($account->getBank()->getUser() == $user) {
-            $adapter = new DoctrineCollectionAdapter($account->getSchedulers());
+            $params = array(
+                ':account_id' => $account->getAccountId()
+            );
+
+            $sql = 'SELECT
+                scheduler.scheduler_id AS scheduler_id,
+                scheduler.third_party AS scheduler_third_party,
+                scheduler.debit AS scheduler_debit,
+                scheduler.credit AS scheduler_credit,
+                scheduler.value_date AS scheduler_value_date,
+                scheduler.limit_date AS scheduler_limit_date,
+                scheduler.is_reconciled AS scheduler_is_reconciled,
+                scheduler.notes AS scheduler_notes,
+                scheduler.frequency_unit AS scheduler_frequency_unit,
+                scheduler.frequency_value AS scheduler_frequency_value,
+                scheduler.is_active AS scheduler_is_active,
+                account.account_id AS account_id,
+                account.name AS account_name,
+                account.currency AS account_currency,
+                transfer_account.account_id AS transfer_account_id,
+                transfer_account.name AS transfer_account_name,
+                category.category_id AS category_id,
+                category.name AS category_name,
+                payment_method.payment_method_id AS payment_method_id,
+                payment_method.name AS payment_method_name
+                ';
+            $sql.= 'FROM scheduler ';
+            $sql.= 'INNER JOIN account ON scheduler.account_id = account.account_id ';
+            $sql.= 'LEFT JOIN account AS transfer_account ON scheduler.transfer_account_id = transfer_account.account_id ';
+            $sql.= 'LEFT JOIN category ON scheduler.category_id = category.category_id ';
+            $sql.= 'LEFT JOIN payment_method ON scheduler.payment_method_id = payment_method.payment_method_id ';
+            $sql.= 'WHERE scheduler.account_id = :account_id ';
+            $sql.= 'ORDER BY scheduler.created_at DESC ';
+
+            $conn = $this->em->getConnection();
+
+            $getNbResultsCallback = function() use ($sql, $conn, $params) {
+                $sql = 'SELECT COUNT(*) AS total ' . substr($sql, strpos($sql, ' FROM '), strlen($sql));
+
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($params);
+
+                return $stmt->fetchColumn();
+            };
+
+            $getSliceCallback = function($offset, $length) use ($sql, $conn, $params) {
+                $sql.= 'LIMIT ' . $offset . ', ' . $length;
+
+                $stmt = $conn->prepare($sql);
+                $stmt->execute($params);
+
+                $schedulers = array();
+
+                foreach ($stmt->fetchAll() as $row) {
+                    if (!isset($schedulers[$row['scheduler_id']])) {
+                        $schedulers[$row['scheduler_id']] = array(
+                            'schedulerId' => $row['scheduler_id'],
+                            'account' => array(
+                                'accountId' => $row['account_id'],
+                                'currency' => $row['account_currency'],
+                                'name' => $row['account_name'],
+                            ),
+                            'transferAccount' => array(
+                                'accountId' => $row['transfer_account_id'],
+                                'name' => $row['transfer_account_name'],
+                            ),
+                            'category' => array(
+                                'categoryId' => $row['category_id'],
+                                'name' => $row['category_name'],
+                            ),
+                            'paymentMethod' => array(
+                                'paymentMethodId' => $row['payment_method_id'],
+                                'name' => $row['payment_method_name'],
+                            ),
+
+                            'thirdParty' => $row['scheduler_third_party'],
+                            'debit' => $row['scheduler_debit'],
+                            'credit' => $row['scheduler_credit'],
+                            'amount' => (0 != $row['scheduler_credit']) ? $row['scheduler_credit'] : -$row['scheduler_debit'],
+                            'valueDate' => (null !== $row['scheduler_value_date']) ? new \DateTime($row['scheduler_value_date']) : null,
+                            'limitDate' => (null !== $row['scheduler_limit_date']) ? new \DateTime($row['scheduler_limit_date']) : null,
+                            'reconciled' => $row['scheduler_is_reconciled'],
+                            'notes' => $row['scheduler_notes'],
+                            'frequencyUnit' => $row['scheduler_frequency_unit'],
+                            'frequencyValue' => $row['scheduler_frequency_value'],
+                            'active' => $row['scheduler_is_active'],
+                        );
+                    }
+                }
+
+                return $schedulers;
+            };
+
+            $adapter = new CallbackAdapter($getNbResultsCallback, $getSliceCallback);
 
             $pager = new Pagerfanta($adapter);
             $pager->setMaxPerPage(20);
