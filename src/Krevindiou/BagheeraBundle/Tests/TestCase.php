9afbf88660a6daa399370896c07d1c5c9e1b7c30
krevindiou <krevindiou@users.noreply.github.com>
2013-08-12 22:23:56 +0200

    Need a schema update after create

--- src/Krevindiou/BagheeraBundle/Tests/TestCase.php
+++ src/Krevindiou/BagheeraBundle/Tests/TestCase.php
@@ -31,6 +31,7 @@ class TestCase extends WebTestCase
 
         $this->runConsole('doctrine:schema:drop', array('--force' => null));
         $this->runConsole('doctrine:schema:create');
+        $this->runConsole('doctrine:schema:update', array('--force' => null));
         $this->runConsole('doctrine:fixtures:load', array('--append' => null));
     }
 
