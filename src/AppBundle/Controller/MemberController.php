d4ffc358f205fc7b474995ab06d96a9264446376
krevindiou <krevindiou@users.noreply.github.com>
2015-04-30 00:26:37 +0200

    Remove useless use statement

--- src/AppBundle/Controller/MemberController.php
+++ src/AppBundle/Controller/MemberController.php
@@ -6,7 +6,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
