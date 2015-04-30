9f2bbc474ee36b852acdfbb46a79d7a744e8ed10
krevindiou <krevindiou@users.noreply.github.com>
2015-04-30 21:08:31 +0200

    Use Symfony 2.6 front controller

--- web/app.php
+++ web/app.php
@@ -5,8 +5,9 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-$loader = new ApcClassLoader('bagheera', $loader);
-$loader->register(true);
+$apcLoader = new ApcClassLoader('bagheera', $loader);
+$loader->unregister();
+$apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
@@ -14,7 +15,9 @@ require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
-Request::enableHttpMethodParameterOverride();
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
