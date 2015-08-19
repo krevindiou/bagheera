cf4f6856aaf68b0a32dea477ae0b1dbaddd89a19
krevindiou <krevindiou@users.noreply.github.com>
2015-08-19 23:52:33 +0200

    Hide SQL import output

--- app/Resources/config/docker/postgresql/docker-entrypoint.sh
+++ app/Resources/config/docker/postgresql/docker-entrypoint.sh
@@ -6,8 +6,8 @@ psql -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}'";
 psql -c "CREATE DATABASE ${DB_NAME} WITH OWNER ${DB_USER}"
 psql -c "CREATE DATABASE ${DB_NAME}_test WITH OWNER ${DB_USER}"
 
-psql -U ${DB_USER} -d ${DB_NAME} -a -f /srv/www/bagheera/app/Resources/config/docker/postgresql/structure.sql
-psql -U ${DB_USER} -d ${DB_NAME} -a -f /srv/www/bagheera/app/Resources/config/docker/postgresql/fixtures.sql
+psql -U ${DB_USER} -d ${DB_NAME} -f /srv/www/bagheera/app/Resources/config/docker/postgresql/structure.sql > /dev/null
+psql -U ${DB_USER} -d ${DB_NAME} -f /srv/www/bagheera/app/Resources/config/docker/postgresql/fixtures.sql > /dev/null
 
 pg_ctl -w stop
 
