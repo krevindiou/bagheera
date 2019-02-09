f859f6ae55cc88d8da1404e95b2752a7df760c03
krevindiou <krevindiou@users.noreply.github.com>
2019-02-09 15:47:42 +0100

    Use APP_ENV instead of ENV

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -f /etc/nginx/conf.d/default.conf
-ln -s "/etc/nginx/bagheera.${ENV}.conf" /etc/nginx/conf.d/default.conf
+ln -s "/etc/nginx/bagheera.${APP_ENV}.conf" /etc/nginx/conf.d/default.conf
 
 exec "$@"
