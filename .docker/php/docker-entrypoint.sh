c389caa87d31ff4efd3e7a8edd4ae39c98b9e508
krevindiou <krevindiou@users.noreply.github.com>
2020-12-21 17:58:10 +0100

    Set up file permissions

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -4,4 +4,7 @@ if [[ $APP_ENV != 'dev' ]]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
+setfacl -R -m u:www-data:rwX -m u:`whoami`:rwX var
+setfacl -dR -m u:www-data:rwX -m u:`whoami`:rwX var
+
 exec "$@"
