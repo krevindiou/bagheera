07937e97951b1a6eaf4d9fe3ecac76ea50f636b6
krevindiou <krevindiou@users.noreply.github.com>
2021-09-29 11:34:45 +0200

    Upgrade to PHP-CS-Fixer 3.1

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,14 +11,15 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+$config
     ->setRiskyAllowed(true)
     ->setRules([
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
         '@PHP80Migration' => true,
         '@PHP80Migration:risky' => true,
-        '@PHPUnit60Migration:risky' => true,
+        '@PHPUnit84Migration:risky' => true,
         'backtick_to_shell_exec' => true,
         'blank_line_before_statement' => true,
         'heredoc_indentation' => true,
@@ -29,3 +30,5 @@ return PhpCsFixer\Config::create()
     ])
     ->setFinder($finder)
 ;
+
+return $config;
