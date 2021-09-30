fcfad5df517601b23c8a3751ab6fbb148736a82b
krevindiou <krevindiou@users.noreply.github.com>
2021-09-30 18:47:11 +0200

    Install JWT bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
