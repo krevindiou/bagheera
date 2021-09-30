e6497759b34731a7be2df87a1e74272b632fc0fe
krevindiou <krevindiou@users.noreply.github.com>
2021-09-30 17:14:33 +0200

    Install API Platform

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
