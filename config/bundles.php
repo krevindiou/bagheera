29f143df6b2ef7e2f110366ee23c3bd76c353f0e
krevindiou <krevindiou@users.noreply.github.com>
2021-09-30 18:47:11 +0200

    Install JWT bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
