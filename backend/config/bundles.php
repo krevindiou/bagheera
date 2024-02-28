f60795e17935ed1dd00bf159c5376a0de4f1704a
krevindiou <krevindiou@users.noreply.github.com>
2024-02-28 22:35:37 +0100

    Install ORM Pack

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,10 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => [
         'dev' => true,
     ],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => [
+        'all' => true,
+    ],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => [
+        'all' => true,
+    ],
 ];
