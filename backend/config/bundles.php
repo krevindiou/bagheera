9de28b3de1218462fd553662b0b0d961ccf28a59
krevindiou <krevindiou@users.noreply.github.com>
2024-02-28 22:24:26 +0100

    Install MakerBundle

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -6,4 +6,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [
         'all' => true,
     ],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => [
+        'dev' => true,
+    ],
 ];
